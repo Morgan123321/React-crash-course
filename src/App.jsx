382f5449a99d1e38b8e4805fb05a16860087b97d
Morgan123321 <morgan.feeney1022@gmail.com>
2026-06-22 18:25:49 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+import Todo from './components/TODO.jsx'
+
+function App() {
+  return (
+    <div>
+    <h1>My Todo List</h1>
+
+    <div>
+    <h2>Finish Frontend Simplified</h2>
+    <button>Delete</button>
+    </div>
+      
+    <div>
+    <h2>Finish the interview section </h2>
+    <button>Delete</button>
+    </div>
+
+      <div>
+    <h2>Land a 100k job</h2>
+    <button>Delete</button>
+    </div>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TODO.jsx
@@ -0,0 +1,6 @@
+function Todo() {
+    return(
+        <div>Todo</div>
+    )
+}
+export default Todo
